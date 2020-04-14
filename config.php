4ce078f3ef8228e0dfaa2ba8338b733017aa596c
Michael Berezovsky <pomodorka3@gmail.com>
2020-04-14 15:38:55 +0200

    Initializing repository

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+defined('VG_ACCESS') or die('access denied');
+
+const SITE_URL = 'http://localhost';
+const PATH = '/drive/';
+
+const HOST = 'localhost';
+const USER = 'root';
+const PASS = '';
+const DB_NAME = 'shop';
